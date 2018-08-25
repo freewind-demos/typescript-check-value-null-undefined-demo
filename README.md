7f4037c8343b3a3d420f0ff5e2c0c94758b8116b
freewind <nowindlee@gmail.com>
2018-08-26 00:31:50 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-TypeScript Base Demo
-====================
+TypeScript Check Value `null` or `undefined` Demo
+=================================================
 
 ```
 npm install
 npx ts-node hello.ts
+```
+
+```
+a == null
+a is undefined (typeof(a) === 'undefined')
+b == null
+b is null (b === null)
 ```
--- hello.ts
+++ hello.ts
@@ -1,1 +1,17 @@
-console.log("Hello, Typescript");
+const a = undefined
+const b = null
+
+function check(value: any, name: string) {
+    if (value == null) {
+        console.log(name + ' == null')
+    }
+    if (value === null) {
+        console.log(name + ' is null (' + name + ' === null)')
+    }
+    if (typeof(value) === 'undefined') {
+        console.log(name + ' is undefined (typeof(' + name + ') === \'undefined\')')
+    }
+}
+
+check(a, 'a')
+check(b, 'b')
